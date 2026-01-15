5b51bad8d4ec25a02bb458bd66cb21bf59f0d4ac
vidhya-research <138569694+vidhya-research@users.noreply.github.com>
2026-01-15 19:02:01 +1100

    Initial commit

---
+++ app.py
@@ -0,0 +1,82 @@
+from helper_functions import plan, update
+import streamlit as st
+
+#https://fonts.google.com/icons?icon.set=Material+Symbols&icon.style=Rounded - Refer this website for the icons used.
+
+if 'ss_btn_view' not in st.session_state:
+    st.session_state['ss_btn_view'] = False
+if 'ss_sel_view' not in st.session_state:
+    st.session_state['ss_sel_view'] = False
+if 'ss_btn_update' not in st.session_state:
+    st.session_state['ss_btn_update'] = False
+if 'ss_sel_update' not in st.session_state:
+    st.session_state['ss_sel_update'] = False
+if 'ss_text_update' not in st.session_state:
+    st.session_state["ss_text_update"]=False
+if 'ss_btn_plan' not in st.session_state:
+    st.session_state['ss_btn_plan'] = False
+
+def btn_view():
+    st.session_state['ss_btn_view'] = True
+    st.session_state['ss_btn_update'] = False
+    st.session_state['ss_sel_update'] = False
+    st.session_state['ss_btn_plan'] = False
+    st.session_state['ss_text_update'] = False
+def sel_view():
+    st.session_state['ss_btn_view'] = True
+    st.session_state['ss_sel_view'] = True
+def btn_update():
+    st.session_state['ss_btn_view'] = False
+    st.session_state['ss_btn_update'] = True
+    st.session_state['ss_btn_plan'] = False
+def sel_update():
+    st.session_state['ss_btn_update'] = True
+    st.session_state['ss_sel_update'] = True
+def text_update():
+    st.session_state['ss_btn_update'] = True
+    st.session_state['ss_sel_update'] = True
+    st.session_state['ss_text_update'] = True
+def btn_plan():
+    st.session_state['ss_btn_view'] = False
+    st.session_state['ss_btn_update'] = False
+    st.session_state['ss_btn_plan'] = True
+
+    
+st.set_page_config(layout="wide")
+st.title('Flavours of home')
+
+files = ['breakfast','lunch','veggies','fruits','dinner','curries','chutney']
+
+st.button(label="Let's view the menu!",help="Please click this button to view the menu!",icon=":material/multimodal_hand_eye:",on_click=btn_view)
+
+if st.session_state['ss_btn_view'] == True:
+    option1 = st.selectbox(label="Please select the menu!",options=files,index=None,on_change=sel_view)
+    if option1!=None:
+        st.write("Menu for ",option1)
+        file_name = option1+".txt"
+        with open(file_name,"r") as f:
+            lst = f.readlines()
+        s=""
+        for i in lst:
+            s += "- "+i+"\n"
+        st.markdown(s)
+
+st.button(label="Let's update the menu!", help="Please click this button to add more options to the menu!", icon=":material/edit_document:",on_click=btn_update)
+
+if st.session_state["ss_btn_update"] == True:
+    option1 = st.selectbox(label='Please select the menu to be updated!', options=files,index=None,on_change=sel_update)
+    if st.session_state["ss_sel_update"] == True:
+        if option1 != None:
+            option2 = st.text_input(label='Please type the flavour to be added!',on_change=text_update)
+            if st.session_state["ss_text_update"]==True:
+                str = update(option1, option2)
+                st.write(str)
+
+if st.button(label="Let's Plan for the week!", help="Please click this button to plan the flavours for the week!", icon=":material/chef_hat:",on_click=btn_plan):
+    df = plan()
+    st.dataframe(df)
+
+
+
+
+
---
+++ helper_functions.py
@@ -0,0 +1,76 @@
+import random
+import pandas as pd
+
+def update(option1, option2):
+    file_name=option1+".txt"
+    with open(file_name,"r") as f:
+        f.seek(0)
+        lst=f.readlines()
+        c = 0
+        for i in lst:
+            if i.strip() == option2.strip():
+                break
+            c+=1
+        if c == len(lst):
+            with open(file_name,"a") as f:
+                f.write("\n"+option2)
+            str = option2+" Added!"
+            return(str)
+        else:
+            str=option2+" Already Exists!"
+            return(str)
+
+def plan():
+    with open("lunch.txt","r") as f:
+        lunch = f.readlines()
+    with open("veggies.txt","r") as f:
+        veggies = f.readlines()
+    with open("fruits.txt","r") as f:
+        fruits = f.readlines()
+    with open("breakfast.txt","r") as f:
+        breakfast = f.readlines()
+    with open("dinner.txt","r") as f:
+        dinner = f.readlines()
+    with open("curries.txt","r") as f:
+        curries = f.readlines()
+    with open("chutney.txt","r") as f:
+        chutney = f.readlines()
+    len_lunch = len(lunch)
+    len_veggies = len(veggies)
+    len_fruits = len(fruits)
+    len_breakfast = len(breakfast)
+    len_dinner = len(dinner)  
+    len_curries = len(curries)  
+    len_chutney = len(chutney)
+    lst_lunch = random.sample(range(0,len_lunch),7)
+    lst_veggies = random.sample(range(0,len_veggies),7)
+    lst_fruits = random.sample(range(0,len_fruits),3)
+    lst_breakfast = random.sample(range(0,len_breakfast),7)
+    
+    dict_lunch = {}
+    dict_fruits = {}
+    dict_breakfast = {}
+    dict_dinner = {}    
+    days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    for i in range(7):
+        r_curries = random.choice(range(len_curries))
+        r_chutney = random.choice(range(len_chutney))
+        if lst_breakfast[i] >=4 and lst_breakfast[i] <=6:                                           #Change if change in breakfast.txt 
+            dict_breakfast[days[i]] = breakfast[lst_breakfast[i]] + ", " + curries[r_curries]
+        elif lst_breakfast[i] <= 3:                                                                 #Change if change in breakfast.txt
+            dict_breakfast[days[i]] = breakfast[lst_breakfast[i]] + ", " + chutney[r_chutney]
+        else:
+            dict_breakfast[days[i]] = breakfast[lst_breakfast[i]]
+        dict_lunch[days[i]] = lunch[lst_lunch[i]] + ", " + veggies[lst_veggies[i]]
+        dict_fruits[days[i]] = fruits[random.choice(lst_fruits)]
+        r_dinner = random.choice(range(len_dinner) )
+        if r_dinner >= 2 and r_dinner <= 3:                                                                      #Change if change in dinner.txt
+            dict_dinner[days[i]] = dinner[r_dinner] + ", " + curries[r_curries]
+        elif r_dinner <= 1:                                                                                     #Change if change in dinner.txt
+            dict_dinner[days[i]] = dinner[r_dinner] + ", " + chutney[r_chutney]
+        else:
+            dict_dinner[days[i]] = dinner[r_dinner]
+    lst_of_dict = [dict_breakfast,dict_lunch,dict_fruits,dict_dinner]
+    df = pd.DataFrame(data=lst_of_dict, index=["Breakfast","Lunch","Snack","Dinner"])
+    return df
+    
